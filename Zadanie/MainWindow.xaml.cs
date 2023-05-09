0bdb9fc39b0afcc2fdcfbbcf7cfa82f73152d5ec
Markson <88060437+Markson-bit@users.noreply.github.com>
2023-05-09 14:36:16 +0200

    Added database, rewrited methods to work with it

--- Zadanie/MainWindow.xaml.cs
+++ Zadanie/MainWindow.xaml.cs
@@ -15,6 +15,9 @@ using System.Windows.Shapes;
 using System.Data;
 using Microsoft.Win32;
 using System.IO;
+using System.Data.SQLite;
+using System.Xml.Linq;
+using CsvHelper;
 
 
 namespace Zadanie
@@ -22,108 +25,88 @@ namespace Zadanie
     public partial class MainWindow : Window
     {
 
+        // Deklaracja wlasciwosci, ktore przechowuja sciezki do plikow i do bazy danych
         public string FilePath { get; set; }
         public string ItemsPath { get; set; }
+        public string DatabasePath { get; set; }
 
-        private DataTable SecondDataTable;
+        private readonly DataTable SecondDataTable;
 
         public MainWindow()
         {
             InitializeComponent();
+
             SecondDataTable = new DataTable();
             DataContext = this;
-            ItemsPath = AppDomain.CurrentDomain.BaseDirectory + @"/DocumentItems.csv";
+
+            ItemsPath = AppDomain.CurrentDomain.BaseDirectory + @"..\\..\\\data\DocumentItems.csv";
+            FilePath = AppDomain.CurrentDomain.BaseDirectory + @"..\\..\\\data\Documents.csv";
+            DatabasePath = AppDomain.CurrentDomain.BaseDirectory + @"..\\..\\\data\database.db";
+          
+            // Wyswietlanie sciezki do pliku Documents
+            CurrentDirectory.Text = FilePath.ToString();
+
         }
 
+        // Metoda obsglugujaca przycisk minimalizowania okna
         private void MinimizeButton_Click (object sender, RoutedEventArgs e)
         {
             WindowState = WindowState.Minimized;
         }
 
+        // Metoda obsglugujaca przycisk zamkniecie okna aplikacji
         private void ExitButton_Click(object sender, RoutedEventArgs e)
         {
             Close();
         }
 
-        private void LocationButton_Click(object sender, RoutedEventArgs e)
-        {
-            OpenFileDialog openFileDialog = new OpenFileDialog();
-            if (openFileDialog.ShowDialog() == true)
-            {
-                FilePath = openFileDialog.FileName;
-                CurrentDirectory.Text = FilePath.ToString();
-
-                var dataTable = new DataTable();
-                using (var streamReader = new StreamReader(openFileDialog.FileName))
-                {
-                    var isFirstLine = true;
-                    while (!streamReader.EndOfStream)
-                    {
-                        var line = streamReader.ReadLine();
-                        if (string.IsNullOrWhiteSpace(line)) continue;
-
-                        var values = line.Split(';');
-                        if (isFirstLine)
-                        {
-                            isFirstLine = false;
-                            foreach (var value in values)
-                            {
-                                dataTable.Columns.Add(new DataColumn(value.Trim()));
-                            }
-                        }
-                        else
-                        {
-                            var row = dataTable.NewRow();
-                            for (var i = 0; i < values.Length; i++)
-                            {
-                                row[i] = values[i].Trim();
-                            }
-                            dataTable.Rows.Add(row);
-                        }
-                    }
-                }
-                dataGrid.ItemsSource = dataTable.DefaultView;
-
-            }
-        }
-
+        // Metoda obslugujaca zmiane wiersza w "lewej" tabeli
         private void dataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             int id = 0;
             if (dataGrid.SelectedItem != null)
             {
-                DataRowView row = (DataRowView)dataGrid.SelectedItem;
-                id = Convert.ToInt32(row[0]);
+                // Wychwytywanie potencjalnego bledu podczas wyboru np. pustego wiersza
+                try
+                {
+                    // Zapisanie do zmiennej zawartosci wybranego wiersza z kolumny 1
+                    DataRowView row = (DataRowView)dataGrid.SelectedItem;
+                    id = Convert.ToInt32(row[0]);
+                }
+                catch { };
             }
 
             CurrentRow.Text = id.ToString();
 
+            // Zmienna do przechowywania wartosci w formie tabelarycznej
             var itemsTable = new DataTable();
-            using (var streamReader = new StreamReader(ItemsPath))
-            {
-                var isFirstLine = true;
-                while (!streamReader.EndOfStream)
-                {
-                    var line = streamReader.ReadLine();
-                    if (string.IsNullOrWhiteSpace(line)) continue;
 
-                    var values = line.Split(';');
-                    if (isFirstLine)
+            // Polaczenie z baza danych
+            using (var connection = new SQLiteConnection($"Data Source={DatabasePath}"))
+            {
+                connection.Open();
+                using (var command = new SQLiteCommand(connection))
+                {
+                    // Pobranie z bazy danych zawartosci wierszy, ktorych atrybut DocumentId jest rowny wybranemu id wiersza, pomijajac pierwszy wiersz, ktory jest naglowkiem
+                    command.CommandText = $"SELECT * FROM Item WHERE DocumentId = {id}";
+                    using (var reader = command.ExecuteReader())
                     {
-                        isFirstLine = false;
-                        foreach (var value in values)
+                        var isFirstResult = true;
+                        while (reader.Read())
                         {
-                            itemsTable.Columns.Add(new DataColumn(value.Trim()));
-                        }
-                    }
-                    else
-                    {
-                        var row = itemsTable.NewRow();
-                        if (Convert.ToInt32(values[0].Trim()) == id) // dodaj warunek
-                        {
-                            for (var i = 0; i < values.Length; i++)
+                            if (isFirstResult)
                             {
-                                row[i] = values[i].Trim();
+                                isFirstResult = false;
+                                for (var i = 0; i < reader.FieldCount; i++)
+                                {
+                                    itemsTable.Columns.Add(new DataColumn(reader.GetName(i).Trim()));
+                                }
+                            }
+
+                            var row = itemsTable.NewRow();
+                            for (var i = 0; i < reader.FieldCount; i++)
+                            {
+                                row[i] = reader.GetValue(i).ToString().Trim();
                             }
                             itemsTable.Rows.Add(row);
                         }
@@ -131,8 +114,156 @@ namespace Zadanie
                 }
             }
 
+            // Zmiana zrodla tabeli 
             detailsGrid.ItemsSource = itemsTable.DefaultView;
+        }
 
+        // Metoda obslugujaca przycisk Save
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Usuwanie zawartosci datagrid
+            ((DataView)dataGrid.ItemsSource).Table.Clear();
+
+            var existingIds = new HashSet<string>();
+            using (var connection = new SQLiteConnection($"Data Source={DatabasePath};Version=3;"))
+            {
+                connection.Open();
+                var selectCommand = new SQLiteCommand("SELECT ID FROM Document", connection);
+                using (var reader = selectCommand.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        existingIds.Add(reader.GetValue(0).ToString());
+                    }
+                }
+            }
+
+            using (var streamReader = new StreamReader(FilePath))
+            {
+                streamReader.ReadLine();
+
+                while (!streamReader.EndOfStream)
+                {
+                    var line = streamReader.ReadLine();
+                    var values = line.Split(';');
+
+                    var id = values[0];
+                    var name = values[1];
+                    var date = values[2];
+                    var firstName = values[3];
+                    var lastName = values[4];
+                    var city = values[5];
+
+                    if (existingIds.Contains(id))
+                    {
+                        // Skip adding record if ID already exists
+                        continue;
+                    }
+
+                    using (var connection = new SQLiteConnection($"Data Source={DatabasePath};Version=3;"))
+                    {
+                        connection.Open();
+                        var command = new SQLiteCommand("INSERT INTO Document VALUES (@id, @name, @date, @firstName, @lastName, @city)", connection);
+                        command.Parameters.AddWithValue("@id", id);
+                        command.Parameters.AddWithValue("@name", name);
+                        command.Parameters.AddWithValue("@date", date);
+                        command.Parameters.AddWithValue("@firstName", firstName);
+                        command.Parameters.AddWithValue("@lastName", lastName);
+                        command.Parameters.AddWithValue("@city", city);
+                        command.ExecuteNonQuery();
+                    }
+                }
+            }
+
+            var existingDocumentIds = new HashSet<string>();
+            using (var connection = new SQLiteConnection($"Data Source={DatabasePath};Version=3;"))
+            {
+                connection.Open();
+                var selectCommand = new SQLiteCommand("SELECT DocumentId FROM Item", connection);
+                using (var reader = selectCommand.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        existingDocumentIds.Add(reader.GetValue(0).ToString());
+                    }
+                }
+            }
+
+            using (var streamReader = new StreamReader(ItemsPath))
+            {
+                streamReader.ReadLine();
+
+                while (!streamReader.EndOfStream)
+                {
+                    var line = streamReader.ReadLine();
+                    var values = line.Split(';');
+
+                    var documentId = values[0];
+                    var ordinal = values[1];
+                    var product = values[2];
+                    var quantity = values[3];
+                    var price = values[4];
+                    var taxRate = values[5];
+
+                    if (existingDocumentIds.Contains(documentId))
+                    {
+                        // Skip adding record if ID already exists
+                        continue;
+                    }
+
+                    using (var connection = new SQLiteConnection($"Data Source={DatabasePath};Version=3;"))
+                    {
+                        connection.Open();
+                        var command = new SQLiteCommand("INSERT INTO Item VALUES (@DocumentId, @Ordinal, @Product, @Quantity, @Price, @TaxRate)", connection);
+                        command.Parameters.AddWithValue("@DocumentId", documentId);
+                        command.Parameters.AddWithValue("@Ordinal", ordinal);
+                        command.Parameters.AddWithValue("@Product", product);
+                        command.Parameters.AddWithValue("@Quantity", quantity);
+                        command.Parameters.AddWithValue("@Price", price);
+                        command.Parameters.AddWithValue("@TaxRate", taxRate);
+                        command.ExecuteNonQuery();
+                    }
+                }
+            }
+        }
+
+        // Metoda obslugujaca przycisk Load
+        private void LoadButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Zmienna do przechowywania wartosci w formie tabelarycznej
+            var dataTable = new DataTable();
+
+            // Polaczenie z baza danych
+            using (var connection = new SQLiteConnection($"Data Source={DatabasePath};Version=3;"))
+            {
+                connection.Open();
+
+                // Pobranie zawartosci bazy danych, pomijajac pierwszy wiersz, ktory jest naglowkiem i zapisywanie do zmiennej dataTable
+                var command = new SQLiteCommand("SELECT * FROM Document", connection);
+                using (var reader = command.ExecuteReader())
+                {
+                    var isFirstRow = true;
+                    while (reader.Read())
+                    {
+                        if (isFirstRow)
+                        {
+                            isFirstRow = false;
+                            for (int i = 0; i < reader.FieldCount; i++)
+                            {
+                                dataTable.Columns.Add(new DataColumn(reader.GetName(i)));
+                            }
+                        }
+                        var row = dataTable.NewRow();
+                        for (int i = 0; i < reader.FieldCount; i++)
+                        {
+                            row[i] = reader.GetValue(i);
+                        }
+                        dataTable.Rows.Add(row);
+                    }
+                }
+            }
+            // Zmiana zrodla tabeli 
+            dataGrid.ItemsSource = dataTable.DefaultView;
         }
     }
 }
